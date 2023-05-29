49120e6ff805c6b5cb316af84686b579e745d478
kyhm <mse1520@naver.com>
2023-05-29 22:53:17 +0900

    Test tailwind form plugin

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,8 +1,11 @@
+const forms = require('@tailwindcss/forms');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [forms],
 };
